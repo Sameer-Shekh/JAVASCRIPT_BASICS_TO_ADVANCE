ee25fb02e05178cea4e4408a0ccd37351691ff58
Sameer Shekh <khansameer7311@gmail.com>
2025-08-24 16:38:24 +0530

    type conversion

---
+++ 2_type_conversion.js
@@ -0,0 +1,72 @@
+// let score = 33
+// let score = "33"
+// let score = null
+// let score = undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // NaN : IN CASE STRING IS 223BFA SOMETHING LIKE THAT and IN CASE OF UNDEFINED
+
+
+// "33" => 33
+// "22ABD" => NaN
+// true => 1
+
+
+// let isLoggedIn = "sameer";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "hitest" => true
+// 33 => true
+
+
+
+// let some = 33;
+// let stringNum = String(some);
+// console.log(stringNum);
+// console.log(typeof stringNum)
+
+// ************************************** Operations ******************************
+// let value = 3;
+// let neg = -value;
+// console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "samer";
+let str2 = "rahul";
+
+let str = str1+str2;
+// console.log(str);
+
+
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+
+console.log("1" + 2 + 2); // 122
+
+
+console.log(1 + 2 + "2"); // 32
+
+// console.log(+true);
+// console.log(+"");
+
+// learn prefix and postfix
+
+
+
+
+
