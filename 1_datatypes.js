942a24c1f82ce375b53a7543f25e5c55b16b3435
Sameer Shekh <khansameer7311@gmail.com>
2025-08-24 16:33:03 +0530

    datatypes

---
+++ 1_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// we are using node js
+
+let name = "sameer"
+let age = 12
+let isLoggedIn = false
+
+
+// number => 2 to power 53 range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  or empty value different type
+// undefined => not assign value
+// symbol => unique
+
+
+// object
+// array
+// functions
+ 
+console.log(typeof "samer")
+console.log(typeof age)
+console.log(typeof null)  
+console.log(typeof undefined)
+
